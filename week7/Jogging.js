36bf0ff84f69c663e9a5e59b7923c55d39dcfa88
be0424 <77755014+be0424@users.noreply.github.com>
2021-02-25 14:28:55 -0500

    Add files via upload

---
+++ week7/Jogging.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react"
+
+class Jogging extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        // Sets miles to 0 by default
+        this.state = {
+            miles: 0
+        }
+    }
+
+    render()
+    {
+        // The first button adds 1 to the miles jogged counter
+        // The second button resets the counter to 0
+        return(
+            <div>
+                <h2>Miles Jogged: {this.state.miles}</h2>
+                <button onClick = {() => this.setState({miles: this.state.miles + 1})}>Mile Jogged</button>
+                <button onClick = {() => this.setState({miles: 0})}>Reset Counter</button>
+            </div>
+        )
+    }
+}
+
+export default Jogging;
---
+++ week7/Weights.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+function Weights()
+{
+    // This sets the variable (reps) and the function (setReps). useState sets the variable to 0
+    const [reps, setReps] = useState(0);
+
+    // The first button adds 1 to the weight lift reps counter
+    // The second button resets the weight lift counter to 0
+    return(
+        <div>
+            <h2>Reps of Weight Lifts: {reps}</h2>
+            <button onClick = {() => setReps(reps + 1)}>Weight Lift Rep</button>
+            <button onClick = {() => setReps(0)}>Reset Counter</button>
+        </div>
+    )
+}
+
+export default Weights;
