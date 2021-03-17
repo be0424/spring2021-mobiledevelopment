cea18c0cde71cef256edd839c6d9a95d90730eb6
be0424 <77755014+be0424@users.noreply.github.com>
2021-03-16 22:14:00 -0400

    Add files via upload

---
+++ week10/Food.js
@@ -0,0 +1,13 @@
+import React, {Component, component} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Food extends Component
+{
+    render() {
+        return(
+            <View>
+                <Text>Spaghetti</Text>
+            </View>
+        )
+    }
+}
---
+++ week10/Name.js
@@ -0,0 +1,13 @@
+import React, {Component, component} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends Component
+{
+    render() {
+        return(
+            <View>
+                <Text>Ben Wilcox</Text>
+            </View>
+        )
+    }
+}
